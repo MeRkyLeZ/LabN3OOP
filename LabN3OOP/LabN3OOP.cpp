ca19a1323b9685d2401f06dccd7e8f4e15adbc7c
MeRkyLeZ <robert.avhadiev.01@mail.ru>
2020-11-07 02:26:01 +0500

    Добавлена функция тестирования
    Добавлена функция тестирования, проведены тесты с измерением времени

--- LabN3OOP/LabN3OOP.cpp
+++ LabN3OOP/LabN3OOP.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <time.h>
+#include <chrono>
 
 using namespace std;
 
@@ -9,20 +11,20 @@ public:
 	Point2D() {
 		x = 0;
 		y = 0;
-		printf("Point2D()\n");
+		//printf("Point2D()\n");
 	}
 	Point2D(double x, double y) {
 		this->x = x;
 		this->y = y;
-		printf("Point2D(double x, double y)\n");
+		//printf("Point2D(double x, double y)\n");
 	}
 	Point2D(const Point2D& point) {
 		this->x = point.x;
 		this->y = point.y;
-		printf("Point2D(const Point2D &point)\n");
+		//printf("Point2D(const Point2D &point)\n");
 	}
 	~Point2D() {
-		printf("~Point2D()\n");
+		//printf("~Point2D()\n");
 	}
 	double getX() {
 		return x;
@@ -33,7 +35,7 @@ public:
 	void setX(double x) {
 		this->x = x;
 	}
-	void SetY(double y) {
+	void setY(double y) {
 		this->y = y;
 	}
 };
@@ -49,14 +51,14 @@ public:
 		count = 0;
 		arr = new Point2D * [size];
 		init();
-		printf("Figure2D()\n");
+		//printf("Figure2D()\n");
 	}
 	Figure2D(unsigned int size) {	// Конструктор
 		this->size = size;
 		count = 0;
 		arr = new Point2D * [size];
 		init();
-		printf("Figure2D(int size)\n");
+		//printf("Figure2D(int size)\n");
 	}
 	Figure2D(const Figure2D& figure) {	// Конструктор копирования
 		arr = new Point2D * [figure.size];
@@ -65,7 +67,7 @@ public:
 		for (int i = 0; i < size; ++i) {
 			arr[i] = new Point2D(*figure.arr[i]);
 		}
-		printf("Figure2D(const Figure2D& figure)\n");
+		//printf("Figure2D(const Figure2D& figure)\n");
 	}
 	~Figure2D() {	// Деструктор
 		for (int i = 0; i < size; ++i) {
@@ -74,7 +76,7 @@ public:
 			arr[i] = 0;
 		}
 		delete[] arr;
-		printf("~Figure2D()\n");
+		//printf("~Figure2D()\n");
 	}
 	void addObject(Point2D* point) {	// Добавление элемента
 		int pos = 0;
@@ -123,8 +125,49 @@ private:
 	}
 };
 
+void Test(int count) {	// Проверка работы
+	// создаем хранилище
+	Figure2D* figure;
+	figure = new Figure2D(count);
+	// добавляем в него объекты
+	for (int i = 0; i < figure->getSize(); ++i)
+		figure->setObject(i, new Point2D());
+	// обращаемся поочередно к элементам
+	for (int i = 0; i < figure->getSize(); ++i) {
+		srand(time(0));
+		int key = rand() % 4 + 1;
+		switch (key) {
+		case 1: figure->getObject(i).setX(rand() % 51); break;
+		case 2: figure->getObject(i).setY(rand() % 51); break;
+		case 3: figure->getObject(rand() % figure->getSize() - 1).getX(); break;
+		case 4: figure->getObject(rand() % figure->getSize() - 1).getY(); break;
+		}
+	}
+	delete figure;
+}
+
+
+
 int main()
 {
-
+	setlocale(LC_ALL, "Russian");
+	cout << "100:" << endl;
+	auto start = chrono::high_resolution_clock::now();	// Начало отсчета времени
+	Test(100);
+	auto end = chrono::high_resolution_clock::now();	// Конец отсчета времени
+	chrono::duration<float> duration = end - start;
+	cout << "Время работы = " << duration.count() << " сек." << endl;
+	start = chrono::high_resolution_clock::now();	// Начало отсчета времени
+	cout << "1000:" << endl;
+	end = chrono::high_resolution_clock::now();	// Конец отсчета времени
+	duration = end - start;
+	Test(1000);
+	cout << "Время работы = " << duration.count() << " сек." << endl;
+	cout << "10000:" << endl;
+	start = chrono::high_resolution_clock::now();	// Начало отсчета времени
+	Test(10000);
+	end = chrono::high_resolution_clock::now();	// Конец отсчета времени
+	duration = end - start;
+	cout << "Время работы = " << duration.count() << " сек." << endl;
 	return 0;
 }
